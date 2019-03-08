5677c1b49e2bf35e679631861e6b17e75d00ca04
Andrew MacLeod <andrewmorrismacleod@googlemail.com>
2019-03-08 13:40:19 +0000

    completing initial questionanswer class

--- server/Sporcleish/src/main/java/com/example/Sporcleish/models/QuestionAnswer.java
+++ server/Sporcleish/src/main/java/com/example/Sporcleish/models/QuestionAnswer.java
@@ -1,11 +1,33 @@
 package com.example.Sporcleish.models;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="questions")
 public class QuestionAnswer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="question")
     private String question;
 
     public QuestionAnswer(String question) {
         this.question = question;
+    }
+
+    public QuestionAnswer(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getQuestion() {
