9c15cf65acd8658c37681beea9e583f1a6862a39
Andrew MacLeod <andrewmorrismacleod@googlemail.com>
2019-03-08 13:33:51 +0000

    adding initial questionanswer class

---
+++ server/Sporcleish/src/main/java/com/example/Sporcleish/models/QuestionAnswer.java
@@ -0,0 +1,18 @@
+package com.example.Sporcleish.models;
+
+public class QuestionAnswer {
+
+    private String question;
+
+    public QuestionAnswer(String question) {
+        this.question = question;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+}
