5739b6b5e375da3d4fd04eb0ed585ffbbba9cf8e
Michael Conner <michaelconner@hotmail.co.uk>
2019-03-08 13:41:18 +0000

    app file amended to point at container

--- client/quiz/src/App.js
+++ client/quiz/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import QuizContainer from './containers/QuizContainer'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Quiz!!!!!!!!!!</h1>
-      </div>
+     <QuizContainer/>
     );
   }
 }
