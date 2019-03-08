4a6266760fa6a01d2adb8b32da3398abe8a73384
Andrew MacLeod <andrewmorrismacleod@googlemail.com>
2019-03-08 10:41:11 +0000

    adding framework base

---
+++ server/Sporcleish/src/main/java/com/example/Sporcleish/SporcleishApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sporcleish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SporcleishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SporcleishApplication.class, args);
+	}
+
+}
---
+++ server/Sporcleish/src/test/java/com/example/Sporcleish/SporcleishApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.Sporcleish;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SporcleishApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
