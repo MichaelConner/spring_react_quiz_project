255a83b1cae5e2af03a299753cf3a7fc1914dff9
Andrew MacLeod <andrewmorrismacleod@googlemail.com>
2019-03-11 10:27:42 +0000

    commented out quiz repository

--- server/Sporcleish/src/main/java/com/example/Sporcleish/repositories/QuizRepositories/QuizRepository.java
+++ server/Sporcleish/src/main/java/com/example/Sporcleish/repositories/QuizRepositories/QuizRepository.java
@@ -2,7 +2,8 @@ package com.example.Sporcleish.repositories.QuizRepositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuizRepository extends JpaRepository<Quiz, Long>, QuizRepositoryCustom {
-
-
-}
+//public interface QuizRepository extends JpaRepository<Quiz, Long>, QuizRepositoryCustom {
+//
+//
+//
+//}
