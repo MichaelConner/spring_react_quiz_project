faa471e213fc11ffc1c7a24cca7fb5798a4f5fc5
Nadja Kelly <nmfort-info@yahoo.de>
2019-03-11 13:07:27 +0000

    question category correction

--- server/Sporcleish/src/main/java/com/example/Sporcleish/components/DataLoader.java
+++ server/Sporcleish/src/main/java/com/example/Sporcleish/components/DataLoader.java
@@ -48,22 +48,22 @@ public class DataLoader implements ApplicationRunner {
         QuestionAnswer Q3  = new QuestionAnswer("Which lake is the world's deepest?", "Baikal",quiz1);
         questionRepository.save(Q3);
 
-        QuestionAnswer Q4  = new QuestionAnswer("Which moon is the largest moon of Jupiter?", "Ganymede",quiz2);
+        QuestionAnswer Q4  = new QuestionAnswer("Which moon is the largest moon of Jupiter?", "Ganymede",quiz3);
         questionRepository.save(Q4);
 
-        QuestionAnswer Q5  = new QuestionAnswer("At what speed does light travel (to the nearest thousand mph)?", "186000", quiz2);
+        QuestionAnswer Q5  = new QuestionAnswer("At what speed does light travel (to the nearest thousand mph)?", "186000", quiz3);
         questionRepository.save(Q5);
 
-        QuestionAnswer Q6  = new QuestionAnswer("Ophidiophobia is the fear of which animal?", "snake", quiz2);
+        QuestionAnswer Q6  = new QuestionAnswer("Ophidiophobia is the fear of which animal?", "snake", quiz3);
         questionRepository.save(Q6);
 
-        QuestionAnswer Q7  = new QuestionAnswer("Born in Giffnock, Scotland in 1951, who served as UK Prime Minister between 2007 and 2010?", "Gordon Brown", quiz3);
+        QuestionAnswer Q7  = new QuestionAnswer("Born in Giffnock, Scotland in 1951, who served as UK Prime Minister between 2007 and 2010?", "Gordon Brown", quiz2);
         questionRepository.save(Q7);
 
-        QuestionAnswer Q8  = new QuestionAnswer("A choral balm inn is an anagram of the name of which US president?", "Abraham Lincoln", quiz3);
+        QuestionAnswer Q8  = new QuestionAnswer("A choral balm inn is an anagram of the name of which US president?", "Abraham Lincoln", quiz2);
         questionRepository.save(Q8);
 
-        QuestionAnswer Q9  = new QuestionAnswer("Who served as the first president of Zimbabwe?", "Canaan Banana", quiz3);
+        QuestionAnswer Q9  = new QuestionAnswer("Who served as the first president of Zimbabwe?", "Canaan Banana", quiz2);
         questionRepository.save(Q9);
 
         quiz1.addQuestionAnswer(Q1);
