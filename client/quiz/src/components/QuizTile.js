de1e159523390e40a144bd30fe1630e40c24d7ad
Gabriele Filigheddu <gabriele.filigheddu@hotmail.com>
2019-03-12 11:20:42 +0000

    button triggers on delete function, function still need to be made

--- client/quiz/src/components/QuizTile.js
+++ client/quiz/src/components/QuizTile.js
@@ -8,7 +8,10 @@ class QuizTile extends Component {
             <div className="quiz-tile">
                 <p>Placeholder for img</p>
                 <h4>{this.props.id}: {this.props.category}</h4>
-                <p><button className="delete-button" type="delete-button" > delete</button></p>
+                <p><button className="delete-button" 
+                        type="delete-button"
+                        onClick={() => {this.props.onDelete(`${this.props.id}`)}}
+                        > delete</button></p>
                 
                 <Link to={`quiz/${this.props.id}`} 
                 onClick={() => {this.props.onChange(`${this.props.id}`)}}
