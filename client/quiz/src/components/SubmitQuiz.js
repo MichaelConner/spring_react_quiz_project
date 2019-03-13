25f74ac0a9099efed210e7af231b75b11492425e
Michael Conner <michaelconner@hotmail.co.uk>
2019-03-13 22:48:28 +0000

    submit page css completed

--- client/quiz/src/components/SubmitQuiz.js
+++ client/quiz/src/components/SubmitQuiz.js
@@ -1,24 +1,43 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class SubmitQuiz extends Component{
 
     render(){
         return(
-            <div>
-                <form onSubmit={this.props.submitQuiz}>
-                <h1>Quiz Submit Form</h1>
+            <div className='submit-form-page'>
+           
+                <p></p>
+                <form className="submit-form" onSubmit={this.props.submitQuiz}>
+                <h1>Submit your quiz details below:</h1>
+                <p>
+                <label>What category is your quiz?</label>
                 <input id="category" name="category"></input>
+                </p>
+
+                <p>
+                <label>What image would you like to represent your quiz?</label>
                 <input id="imgurl" name="imgurl"></input>
+                </p>
 
+                <p>
+                <label>What is your first question?</label>
                 <input id="question1" name="question1"></input>
-                <input id="answer1" name="answer1"></input>
+                </p>
 
-                <button type="text">Submit</button>
+                <p>
+                <label>What is the answer to your first question?</label>
+                <input id="answer1" name="answer1"></input>
+                </p>
+
+                <button type="text" className='submit-quiz-button'>Submit</button>
+                <h4><Link to="/">oo| surface back to the home page</Link></h4>
                 </form>
+                <p></p>
+            
             </div>
         )
     }
-
 }
 
 export default SubmitQuiz;
